48a9dad790070eaeed66db0a1fdb009fa0b1b19b
Oleg <himynameisoleg@MacBeezy.local>
2017-08-15 20:51:09 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <boo></boo>
+    <tabletop id="tabletop"></tabletop>
+  </div>
+</template>
+
+<script>
+import Boo from './components/Boo'
+import Tabletop from './components/Tabletop'
+
+export default {
+  name: 'app',
+  components: {
+    Boo,
+    Tabletop
+  }
+}
+</script>
+
+<style>
+* {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#tabletop {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+</style>
---
+++ src/components/Boo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="boo">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'boo',
+  data () {
+    return {
+      msg: 'dungeon'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
